100d5d8a044d09505d1081c98e86364367eeac26
עדן <eden@h-MacBook-Pro-sl-dn.local>
2014-11-02 22:28:49 +0200

    added new submit view

--- HairStyle/src/com/example/hairstyle/MainActivity.java
+++ HairStyle/src/com/example/hairstyle/MainActivity.java
@@ -18,70 +18,74 @@ import com.parse.ParseObject;
 import android.widget.*;
 
 public class MainActivity extends Activity {
-	
+
 	private static final String TAG = MainActivity.class.getSimpleName();
-	 
-	  
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Parse.initialize(this, "3lThhxtk9VqqxSBeRJaZBN1AxdO2bA7u5b8lCBlQ", "VbJDWibi5M3zEKKlP7OxHhJYjU9uhPKeMwP4k8on");
-        CheckBox repeatChkBx = ( CheckBox ) findViewById( R.id.checkBox1 );
-        repeatChkBx.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
-            {
-                if (isChecked)
-                {
-                	buttonView.setEnabled(false); // disable checkbox 
-                }
 
-            }
-        });   
-    }
-   
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Parse.initialize(this, "3lThhxtk9VqqxSBeRJaZBN1AxdO2bA7u5b8lCBlQ",
+				"VbJDWibi5M3zEKKlP7OxHhJYjU9uhPKeMwP4k8on");
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	public void onCheckBoxClicked(View view) {
+		boolean checked = ((CheckBox) view).isChecked();
+		if (checked) {
+			GridLayout grid = (GridLayout) findViewById(R.id.GridLayout1);
+			int childcount = grid.getChildCount();
+			for (int i = 0; i < childcount; i++) {
+				View view2 = grid.getChildAt(i);
+				if (view2 instanceof CheckBox) {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+					view2.setEnabled(false); // disable checkbox
 
-   
-    public void submitTicketCard(View v) {
-    	
-    	int count = 0;
-    	
-    	GridLayout grid = (GridLayout)findViewById(R.id.GridLayout1);
-    	int childcount = grid.getChildCount();
-    	for (int i=0; i < childcount; i++){
-    	      View view2 = grid.getChildAt(i);
-    	      if (view2 instanceof CheckBox) {
-    	    	  CheckBox marked = (CheckBox) view2;
-    	    	  if (marked.isChecked()){
-    	    		  count++;
-    	    	  }  
-    	      }
-    	}
-    	Log.d(TAG, "Count = "+count);
-    	
-    	System.out.println(count);
-    	
-    	  ParseObject ticketObject = new ParseObject("TicketObject");
-    	  ticketObject.put("Number of Pierces", "bar");
-    	  ticketObject.saveInBackground();
-    }
+				}
+			}
+		}
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	public void submitTicketCard(View v) {
+
+		int count = 0;
+
+		GridLayout grid = (GridLayout) findViewById(R.id.GridLayout1);
+		int childcount = grid.getChildCount();
+		for (int i = 0; i < childcount; i++) {
+			View view2 = grid.getChildAt(i);
+			if (view2 instanceof CheckBox) {
+				CheckBox marked = (CheckBox) view2;
+				if (marked.isChecked()) {
+					count++;
+				}
+			}
+		}
+		Log.d(TAG, "Count = " + count);
+
+		System.out.println(count);
+
+		ParseObject ticketObject = new ParseObject("TicketObject");
+		ticketObject.put("Number of Pierces", "bar");
+		ticketObject.saveInBackground();
+	}
 }
---
+++ HairStyle/src/com/example/hairstyle/SubmissionSuccessActivity.java
@@ -0,0 +1,24 @@
+package com.example.hairstyle;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SubmissionSuccessActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+
+	
+	
+}

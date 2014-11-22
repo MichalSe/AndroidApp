c711538453bde06926c6f55c2f06665e8e4a8e3e
עדן <eden@edens.local>
2014-11-22 12:17:17 +0200

    sends the request to hair dresser, load the page with marked checks

---
+++ HairStyle/src/com/example/hairstyle/RequestSuccessActivity.java
@@ -0,0 +1,22 @@
+package com.example.hairstyle;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RequestSuccessActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		setContentView(R.layout.request_success_activity);
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+
+	
+	
+}
